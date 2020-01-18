a5c3aeae20a2b22686900957ab92d507124b63bf
Pragathi Venkatesh <pvenkatesh22@ucla.edu>
2020-01-17 20:18:35 -0800

    restored announcement scroll

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -8,9 +8,26 @@ function limit_height(target_obj, lim_obj) {
 
 function onload_func() {
     when_where_content = document.getElementById("when-where-content")
-    announcements_content = document.getElementById("Announcements-content")
+    announcements_content = document.getElementById("announcements-content")
+    // logo_col = document.getElementById("logo-col")
     limit_height(announcements_content, when_where_content)
+    // limit_height(logo_col, when_where_content)
 }
+
+// function do_collapse(id) {
+//     document.getElementById(id).appendChild("is-collapsed")
+//     element.addAttribute("disabled");
+
+// }
+
+// function do_uncollapse(id) {
+//     document.getElementById(id).appendChild("not-collapsed")
+//     element.removeAttribute("disabled");
+
+// }
+
+// ​document.body.appendChild(inputElement);​
+// }
 
 window.onload = onload_func;
 window.onresize = onload_func;
